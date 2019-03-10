8c55764a2a5564652d5e8c1df37317aaf1c19f69
wrkbr <zcarc1@gmail.com>
2019-03-10 09:51:39 +0900

    Add: ExceptionHandler

---
+++ src/main/java/com/wrkbr/exception/CommonExceptionAdvice.java
@@ -0,0 +1,33 @@
+package com.wrkbr.exception;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+@Log4j
+public class CommonExceptionAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public String exception(Exception e, Model model) {
+        log.error("exception()...");
+        e.printStackTrace();
+
+        model.addAttribute("exception", "에러가 발생했습니다.");
+
+        return "exception/errorPage";
+    }
+
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound(NoHandlerFoundException ex) {
+        log.info("notFound()...");
+        return "exception/error404";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/exception/error404.jsp
@@ -0,0 +1,28 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>error404</title>
+</head>
+
+<br><br><br>
+
+
+<!-- Custom fonts for this template-->
+<link href="resources/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="resources/css/sb-admin-2.min.css" rel="stylesheet">
+
+<body>
+
+<!-- 404 Error Text -->
+<div class="text-center">
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mb-5">Page Not Found</p>
+    <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+    <a href="#">&larr; Back to Dashboard</a>
+</div>
+
+</body>
+</html>
